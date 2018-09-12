9800e74a1db4ac4e108b25804a37fb47c2fbc254
dkesada <dkesada@gmail.com>
2018-09-12 22:10:36 +0200

    Creates the excel report

--- out/out.py
+++ out/out.py
@@ -15,10 +15,39 @@ import pickle
 import datetime
 import os
 from openpyxl import Workbook
+from openpyxl import load_workbook # To do
 
 data_name = 'base.p'
+file_name = datetime.datetime.now().strftime("%d-%m-%Y") + '.xlsx'
 
 class Backend:
 	def __init__(self):
-		a = os.path.dirname(os.path.abspath(__file__))
-		self.base = pickle.load(open(a+"/base.p","rb"))
+		#a = os.path.dirname(os.path.abspath(__file__))
+		self.base = pickle.load(open(data_name,"rb"))
+		self.date = datetime.datetime.today() # .day , .month , .year
+		
+		#Check if the file for today exists
+		if not os.path.isfile(file_name):
+			self.createFile()
+		
+	def createFile(self):
+		wb = Workbook()
+		ws = wb.active		
+		for i in self.base.items():
+			ws['A'+str(i[1][1])] = i[1][0].decode(encoding='utf_8') #badly encoded
+		wb.save(file_name)
+	
+	def check(self, code):
+		"""
+		Check if the code exists
+		"""
+		if code in self.base: return True
+		else: return False
+		
+	
+	def entry(self, code):
+		"""
+		First, check if the entry has already been registered.
+		If not, register the entry
+		"""
+		hora = str(datetime.datetime.now().hour) +':'+ str(datetime.datetime.now().minute)
