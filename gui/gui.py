5393e97df10bac808ff21e8ab6176ab2252e60ea
dkesada <dkesada@gmail.com>
2022-09-04 13:41:16 +0200

    new add/delete functionality

--- gui/gui.py
+++ gui/gui.py
@@ -1,95 +1,174 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
-#author: David Quesada López
+# author: David Quesada López
 
 """
 User interface, let's see how Tkinter goes.
 """
 
-import Tkinter as ttk
+import tkinter as ttk
 
 
 class Gui:
-	def __init__(self, back):
-		self.backend = back
-		self.home = ttk.Tk()
-		
-		self.home.geometry('400x200')
-		self.home.resizable(width=False,height=False)
-		self.home.configure(bg = 'beige')
-		self.home.title('Home')
-		self.canvas = ttk.Frame(self.home)
-		self.inicio()
-		
-		self.home.mainloop()
-	
-	def inicio(self):
-		#Frame que se irá destruyendo
-		self.canvas.pack_forget()
-		self.canvas.destroy()
-		self.canvas = ttk.Frame(self.home)
-		self.canvas.pack()
-		ttk.Button(self.canvas, text='Fichar entrada', command = lambda: self.entrada('Introduzca su código para que su entrada quede registrada')).pack()
-		ttk.Button(self.canvas, text='Fichar salida', command = lambda: self.salida('Introduzca su código para que su salida quede registrada')).pack()
-		ttk.Button(self.canvas, text='Salir', command = self.home.destroy).pack()
-		
-	def entrada(self, t):
-		self.canvas.pack_forget()
-		self.canvas.destroy()
-		self.canvas = ttk.Frame(self.home)
-		self.canvas.pack()
-		
-		up = ttk.Frame(self.canvas)
-		up.pack(side=ttk.TOP)
-		down = ttk.Frame(self.canvas)
-		down.pack(side=ttk.BOTTOM)
-		
-		ttk.Label(up, text=t).pack()
-		ttk.Label(down, text='Código: ').grid(row=0, column=0)
-		entry = ttk.Entry(down, bd = 5)
-		entry.grid(row=0, column=1)
-		ttk.Button(down, text='Fichar', command = lambda: self.processEntry(entry.get())).grid(row=1, column=0)
-		ttk.Button(down, text='Volver', command = self.inicio).grid(row=1, column=1)
-	
-	
-	def processEntry(self, code):
-		"""
-		Takes the code introduced by the user, checks that it is valid and registers the entry
-		"""
-		if self.backend.check(code):
-			if self.backend.entry(code):
-				self.entrada('Su entrada ha quedado registrada, gracias por su tiempo.')
-			else:
-				self.entrada('El código introducido ya ha fichado por hoy.')
-		else:
-			self.entrada('El código introducido no existe. Por favor, introdúzca un código válido.')
-			
-	def salida(self, t):
-		self.canvas.pack_forget()
-		self.canvas.destroy()
-		self.canvas = ttk.Frame(self.home)
-		self.canvas.pack()
-		
-		up = ttk.Frame(self.canvas)
-		up.pack(side=ttk.TOP)
-		down = ttk.Frame(self.canvas)
-		down.pack(side=ttk.BOTTOM)
-		
-		ttk.Label(up, text=t).pack()
-		ttk.Label(down, text='Código: ').grid(row=0, column=0)
-		entry = ttk.Entry(down, bd = 5)
-		entry.grid(row=0, column=1)
-		ttk.Button(down, text='Fichar salida', command = lambda: self.processExit(entry.get())).grid(row=1, column=0)
-		ttk.Button(down, text='Volver', command = self.inicio).grid(row=1, column=1)
-	
-	def processExit(self, code):
-		"""
-		Takes the code introduced by the user, checks that it is valid and registers the entry
-		"""
-		if self.backend.check(code):
-			if self.backend.exit(code):
-				self.salida('Su salida ha quedado registrada, gracias por su tiempo.')
-			else:
-				self.salida('El código introducido ya ha fichado su salida por hoy.')
-		else:
-			self.salida('El código introducido no existe. Por favor, introdúzca un código válido.')
+    def __init__(self, back):
+        self.backend = back
+        self.home = ttk.Tk()
+
+        self.home.geometry('400x200')
+        self.home.resizable(width=False, height=False)
+        self.home.configure(bg='beige')
+        self.home.title('Home')
+        self.canvas = ttk.Frame(self.home)
+        self.inicio()
+
+        self.home.mainloop()
+
+    def inicio(self):
+        # Frame que se irá destruyendo
+        self.canvas.pack_forget()
+        self.canvas.destroy()
+        self.canvas = ttk.Frame(self.home)
+        self.canvas.pack()
+        ttk.Button(self.canvas, text='Fichar entrada',
+                   command=lambda: self.entrada('Introduzca su código para que su entrada quede registrada')).pack()
+        ttk.Button(self.canvas, text='Fichar salida',
+                   command=lambda: self.salida('Introduzca su código para que su salida quede registrada')).pack()
+        ttk.Button(self.canvas, text='Gestionar usuarios',
+                   command=lambda: self.gestion('Elija añadir o eliminar usuarios.')).pack()
+        ttk.Button(self.canvas, text='Salir', command=self.home.destroy).pack()
+
+    def entrada(self, t):
+        self.canvas.pack_forget()
+        self.canvas.destroy()
+        self.canvas = ttk.Frame(self.home)
+        self.canvas.pack()
+
+        up = ttk.Frame(self.canvas)
+        up.pack(side=ttk.TOP)
+        down = ttk.Frame(self.canvas)
+        down.pack(side=ttk.BOTTOM)
+
+        ttk.Label(up, text=t).pack()
+        ttk.Label(down, text='Código: ').grid(row=0, column=0)
+        entry = ttk.Entry(down, bd=5)
+        entry.grid(row=0, column=1)
+        ttk.Button(down, text='Fichar', command=lambda: self.processEntry(entry.get())).grid(row=1, column=0)
+        ttk.Button(down, text='Volver', command=self.inicio).grid(row=1, column=1)
+
+    def processEntry(self, code):
+        """
+        Takes the code introduced by the user, checks that it is valid and registers the entry
+        """
+        if self.backend.check(code):
+            print(code)
+            if self.backend.entry(code):
+                self.entrada('Su entrada ha quedado registrada, gracias por su tiempo.')
+            else:
+                self.entrada('El código introducido ya ha fichado por hoy.')
+        else:
+            self.entrada('El código introducido no existe. Por favor, introdúzca un código válido.')
+
+    def salida(self, t):
+        self.canvas.pack_forget()
+        self.canvas.destroy()
+        self.canvas = ttk.Frame(self.home)
+        self.canvas.pack()
+
+        up = ttk.Frame(self.canvas)
+        up.pack(side=ttk.TOP)
+        down = ttk.Frame(self.canvas)
+        down.pack(side=ttk.BOTTOM)
+
+        ttk.Label(up, text=t).pack()
+        ttk.Label(down, text='Código: ').grid(row=0, column=0)
+        entry = ttk.Entry(down, bd=5)
+        entry.grid(row=0, column=1)
+        ttk.Button(down, text='Fichar salida', command=lambda: self.processExit(entry.get())).grid(row=1, column=0)
+        ttk.Button(down, text='Volver', command=self.inicio).grid(row=1, column=1)
+
+    def processExit(self, code):
+        """
+        Takes the code introduced by the user, checks that it is valid and registers the entry
+        """
+        if self.backend.check(code):
+            if self.backend.exit(code):
+                self.salida('Su salida ha quedado registrada, gracias por su tiempo.')
+            else:
+                self.salida('El código introducido ya ha fichado su salida por hoy.')
+        else:
+            self.salida('El código introducido no existe. Por favor, introdúzca un código válido.')
+
+    def gestion(self, t):
+        self.canvas.pack_forget()
+        self.canvas.destroy()
+        self.canvas = ttk.Frame(self.home)
+        self.canvas.pack()
+
+        up = ttk.Frame(self.canvas)
+        up.pack(side=ttk.TOP)
+        down = ttk.Frame(self.canvas)
+        down.pack(side=ttk.BOTTOM)
+
+        ttk.Label(up, text=t).pack()
+        ttk.Button(self.canvas, text='Añadir',
+                   command=lambda: self.addUser('Introduzca el nombre completo del nuevo usuario.')).pack()
+        ttk.Button(self.canvas, text='Eliminar',
+                   command=lambda: self.delUser('Introduzca el código del usuario a eliminar.')).pack()
+        ttk.Button(self.canvas, text='Volver', command=self.inicio).pack()
+
+    def addUser(self, t):
+        self.canvas.pack_forget()
+        self.canvas.destroy()
+        self.canvas = ttk.Frame(self.home)
+        self.canvas.pack()
+
+        up = ttk.Frame(self.canvas)
+        up.pack(side=ttk.TOP)
+        down = ttk.Frame(self.canvas)
+        down.pack(side=ttk.BOTTOM)
+
+        ttk.Label(up, text=t).pack()
+        ttk.Label(down, text='Nombre completo: ').grid(row=0, column=0)
+        entry = ttk.Entry(down, bd=50)
+        entry.grid(row=0, column=1)
+        ttk.Button(down, text='Añadir', command=lambda: self.processAdd(entry.get())).grid(row=1, column=0)
+        ttk.Button(down, text='Volver', command=self.inicio).grid(row=1, column=1)
+
+    def processAdd(self, name):
+        """
+        Takes the name introduced by the user and adds a new user to the database
+        """
+        code = self.backend.addUser(name)
+        self.backend.deleteFile()
+        self.backend.createFile()
+        self.addUser('Usuario añadido correctamente. Su código es ' + code)
+
+    def delUser(self, t):
+        self.canvas.pack_forget()
+        self.canvas.destroy()
+        self.canvas = ttk.Frame(self.home)
+        self.canvas.pack()
+
+        up = ttk.Frame(self.canvas)
+        up.pack(side=ttk.TOP)
+        down = ttk.Frame(self.canvas)
+        down.pack(side=ttk.BOTTOM)
+
+        ttk.Label(up, text=t).pack()
+        ttk.Label(down, text='Código: ').grid(row=0, column=0)
+        entry = ttk.Entry(down, bd=5)
+        entry.grid(row=0, column=1)
+        ttk.Button(down, text='Eliminar', command=lambda: self.processDel(entry.get())).grid(row=1, column=0)
+        ttk.Button(down, text='Volver', command=self.inicio).grid(row=1, column=1)
+
+    def processDel(self, code):
+        """
+        Takes the code introduced by the user and deletes the corresponding entry in the database
+        """
+        if self.backend.check(code):
+            self.backend.delUser(code)
+            self.backend.deleteFile()
+            self.backend.createFile()
+            self.delUser('Usuario eliminado correctamente.')
+        else:
+            self.delUser('El código introducido no existe. Por favor, introdúzca un código válido.')
--- out/out.py
+++ out/out.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
-#author: David Quesada López
+# author: David Quesada López
 
 """
 Backend. Each time a user arrives, we will show it on an excell file.
@@ -15,74 +15,111 @@ import pickle
 import datetime
 import os
 from openpyxl import Workbook
-from openpyxl import load_workbook # To do
+from openpyxl import load_workbook
+from random import randint
 
 data_name = 'base.p'
-file_name = './informes/'+datetime.datetime.now().strftime("%d-%m-%Y") + '.xlsx'
+file_name = './informes/' + datetime.datetime.now().strftime("%d-%m-%Y") + '.xlsx'
+
 
 class Backend:
-	def __init__(self):
-		#a = os.path.dirname(os.path.abspath(__file__))
-		with open(data_name,'rb') as f:
-			self.base = pickle.load(f)
-		self.date = datetime.datetime.today() # .day , .month , .year
-		
-		#Check if the file for today exists
-		if not os.path.isfile(file_name):
-			self.createFile()
-		
-	def createFile(self):
-		wb = Workbook()
-		ws = wb.active		
-		for i in self.base.items():
-			ws['A'+str(i[1][1])] = i[1][0].decode(encoding='latin1') #badly encoded
-		wb.save(file_name)
-	
-	def check(self, code):
-		"""
+    def __init__(self):
+        # a = os.path.dirname(os.path.abspath(__file__))
+        with open(data_name, 'rb') as f:
+            self.base = pickle.load(f, encoding='latin1')
+        self.date = datetime.datetime.today()  # .day , .month , .year
+
+        # Check if the file for today exists
+        if not os.path.isfile(file_name):
+            self.createFile()
+
+    def createFile(self):
+        wb = Workbook()
+        ws = wb.active
+        for i in self.base.items():
+            ws['A' + str(i[1][1])] = i[1][0]  # .decode(encoding='latin1') #badly encoded
+        wb.save(file_name)
+
+    def deleteFile(self):
+        os.remove(file_name)
+
+    def check(self, code):
+        """
 		Check if the code exists
 		"""
-		if code in self.base: return True
-		else: return False
-		
-	
-	def entry(self, code):
-		"""
+        if code in self.base:
+            return True
+        else:
+            return False
+
+    def entry(self, code):
+        """
 		First, check if the entry has already been registered.
 		If not, register the entry
 		"""
-		ret = False
-		wb = load_workbook(file_name)
-		ws = wb['Sheet']
-		
-		if ws['D'+str(self.base[code][1])].value is None:
-			hora = self.digits(datetime.datetime.now().hour) +':'+ self.digits(datetime.datetime.now().minute)
-			ws['D'+str(self.base[code][1])] = hora
-			wb.save(file_name)
-			ret = True
-		
-		return ret
-		
-	def exit(self, code):
-		"""
+        ret = False
+        wb = load_workbook(file_name)
+        ws = wb['Sheet']
+
+        if ws['D' + str(self.base[code][1])].value is None:
+            hora = self.digits(datetime.datetime.now().hour, 10) + ':' + self.digits(datetime.datetime.now().minute, 10)
+            ws['D' + str(self.base[code][1])] = hora
+            wb.save(file_name)
+            ret = True
+
+        return ret
+
+    def exit(self, code):
+        """
 		First, check if the entry has already been registered.
 		If not, register the entry
 		"""
-		ret = False
-		wb = load_workbook(file_name)
-		ws = wb['Sheet']
-		
-		if ws['E'+str(self.base[code][1])].value is None:
-			hora = self.digits(datetime.datetime.now().hour) +':'+ self.digits(datetime.datetime.now().minute)
-			ws['E'+str(self.base[code][1])] = hora
-			wb.save(file_name)
-			ret = True
-		
-		return ret
-		
-	def digits(self, num):
-		""" Puts '0' before single digit numbers for format purposes """
-		ret = str(num)
-		if num < 10:
-			ret = '0' + str(num)	
-		return ret
+        ret = False
+        wb = load_workbook(file_name)
+        ws = wb['Sheet']
+
+        if ws['E' + str(self.base[code][1])].value is None:
+            hora = self.digits(datetime.datetime.now().hour, 10) + ':' + self.digits(datetime.datetime.now().minute, 10)
+            ws['E' + str(self.base[code][1])] = hora
+            wb.save(file_name)
+            ret = True
+
+        return ret
+
+    @staticmethod
+    def digits(num, k):
+        """ Puts '0' before k digit numbers for format purposes """
+        ret = str(num)
+        if num < k:
+            ret = '0' + str(num)
+        return ret
+
+    def delUser(self, code):
+        """
+		Delete a user from the database
+		"""
+        usr = self.base.pop(code)  # Remove the user
+
+        for i in self.base.keys():  # Lower the position of the rest of the users above it
+            if self.base[i][1] > usr[1]:
+                usr_i = self.base[i]
+                usr_i = (usr_i[0], usr_i[1] - 1)
+                self.base[i] = usr_i
+
+        self.dumpDB()
+
+    def addUser(self, name):
+        """
+        Add a user to the database
+        """
+        code = self.digits(randint(0, 99999), 1e4)
+        while code in self.base.keys():
+            code = self.digits(randint(0, 99999), 1e4)
+        self.base[code] = (name, self.base.__len__() + 1)
+        self.dumpDB()
+
+        return code
+
+    def dumpDB(self):
+        with open(data_name, 'wb') as f:
+            pickle.dump(self.base, f)
