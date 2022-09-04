d4c369bf73a0cb31c47b3ec9f5c54e5b47e7c6f6
dkesada <dkesada@gmail.com>
2022-09-04 14:09:16 +0200

    new setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='EntryTimeKeeper',
+    version='1.0.0',
+    packages=['db', 'gui', 'out'],
+    url='https://github.com/dkesada/EntryTimeKeeper',
+    license='MIT',
+    author='David Quesada',
+    author_email='dkesada@gmail.com',
+    description='Small program to keep a record of the employee\'s entry and exit times.'
+)
